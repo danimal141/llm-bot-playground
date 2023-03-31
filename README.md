e7de9103caccc392052b6a69638f3972a4549cdb
danimal141 <hideaki.ishii1204@gmail.com>
2023-03-31 15:17:25 +0900

    Add lint, formatter

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ $ poetry install
 
 $ poetry shell
 $ OPENAI_API_KEY=xxx streamlit app.py
+
+$ poetry run python -m flake8 **/*.py # lint
+$ poetry run python -m black **/*.py # format
 ```
--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
 
+import openai
 import streamlit as st
 from streamlit_chat import message
-
-import openai
-openai.api_key = os.environ["OPENAI_API_KEY"]
 
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import (
@@ -19,36 +17,35 @@ from langchain.callbacks.base import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.callbacks.streamlit import StreamlitCallbackHandler
 
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
 system_message = """
 あなたはアシスタントです。ユーザはエンジニアで、あなたにプログラミングやプロダクト開発に関する質問を投げかけます。
 アシスタントとして、プロダクト開発に役立つ回答を、できる限り根拠やリファレンスURLを示した上で返してください。"""
 
-prompt = ChatPromptTemplate.from_messages([
-  SystemMessagePromptTemplate.from_template(system_message),
-  MessagesPlaceholder(variable_name="history"),
-  HumanMessagePromptTemplate.from_template("{input}")
-])
+prompt = ChatPromptTemplate.from_messages(
+    [
+        SystemMessagePromptTemplate.from_template(system_message),
+        MessagesPlaceholder(variable_name="history"),
+        HumanMessagePromptTemplate.from_template("{input}"),
+    ]
+)
 
 
 @st.cache_resource
 def load_conversation():
-  llm = ChatOpenAI(
-    streaming=True,
-    callback_manager=CallbackManager([
-      StreamlitCallbackHandler(),
-      StreamingStdOutCallbackHandler()
-    ]),
-    verbose=True,
-    temperature=0,
-    max_tokens=1024
-  )
-  memory = ConversationBufferMemory(return_messages=True)
-  conversation = ConversationChain(
-    memory=memory,
-    prompt=prompt,
-    llm=llm
-  )
-  return conversation
+    llm = ChatOpenAI(
+        streaming=True,
+        callback_manager=CallbackManager(
+            [StreamlitCallbackHandler(), StreamingStdOutCallbackHandler()]
+        ),
+        verbose=True,
+        temperature=0,
+        max_tokens=1024,
+    )
+    memory = ConversationBufferMemory(return_messages=True)
+    conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm)
+    return conversation
 
 
 st.title("エンジニアリングおじさん")
@@ -59,17 +56,16 @@ if "past" not in st.session_state:
     st.session_state.past = []
 
 with st.form("GPTに質問する"):
-  user_message = st.text_area("質問を入力してください")
-  submitted = st.form_submit_button("質問する")
+    user_message = st.text_area("質問を入力してください")
+    submitted = st.form_submit_button("質問する")
 
-  if submitted:
-    conversation = load_conversation()
-    answer = conversation.predict(input=user_message)
+    if submitted:
+        conversation = load_conversation()
+        answer = conversation.predict(input=user_message)
+        st.session_state.past.append(user_message)
+        st.session_state.generated.append(answer)
 
-    st.session_state.past.append(user_message)
-    st.session_state.generated.append(answer)
-
-    if st.session_state["generated"]:
-      for i in range(len(st.session_state.generated) - 1, -1, -1):
-        message(st.session_state.generated[i], key=str(i))
-        message(st.session_state.past[i], is_user=True, key=str(i) + "_user")
+        if st.session_state["generated"]:
+            for i in range(len(st.session_state.generated) - 1, -1, -1):
+                message(st.session_state.generated[i], key=str(i))
+                message(st.session_state.past[i], is_user=True, key=str(i) + "_user")
